032313799b9e5ddd8af6761985b7e045cc08a1c4
Zhiyuan Shi <zhiyuan.shi@gmail.com>
2014-01-25 19:18:54 +0800

    Add server supporting add, modify, and delete operations

--- server/server.rb
+++ server/server.rb
@@ -2,6 +2,13 @@ require 'sinatra'
 require "sinatra/json"
 require 'json'
 require 'mongo'
+require 'bson'
+
+db = Mongo::MongoClient.new.db('rdto')
+comments = db['comments']
+
+comments.create_index('url')
+comments.create_index('comment_id')
 
 configure do
   set :environment, :development
@@ -24,25 +31,32 @@ get '/get' do
 end
 
 # Retrieve all comments at a given URL
-get '/comments/:url' do
-  json()
+get '/page/:url' do
+  json(comments.find('url' => params[:url]).to_a)
+end
+
+get '/comments' do
+  json(comments.find().to_a)
 end
 
 # Add new comment
 post '/comment' do
   request.body.rewind  # in case someone already read it
   data = JSON.parse request.body.read
-  json()
+  # Unverified
+  json(comments.insert(data))
 end
 
-# Modify an existing comment
+# Modify an existing comment identified by a comment_id
 put '/comment/:id' do
   request.body.rewind  # in case someone already read it
-  data = JSON.parse request.body.read
-  json()
+  data = JSON.parse request.body.read, symbolize_names: true
+  json(comments.update({'_id'  => BSON::ObjectId(params[:id])},
+                       {'$set' => {'content' => data[:content]}}
+                      ))
 end
 
 # Delete an existing comment
 delete '/comment/:id' do
-  json()
+  json(comments.remove("_id" => BSON::ObjectId(params[:id])))
 end
